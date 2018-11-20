b1a0cd563c468db13bd7b7f3428a3977d8713a02
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-11-20 11:50:10 +0900

    トップページ用のヘッダーとフッターを実装した。

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    titleTemplate: titleChunk =>
+      titleChunk ? `${titleChunk} - Presentations` : 'Presentations',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <v-app>App</v-app>
+  <v-app>
+    <v-toolbar
+      app
+      flat
+      dark
+      color="primary"
+    >
+      <v-toolbar-title>
+        <h1>Presentations</h1>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        Container
+      </v-container>
+    </v-content>
+
+    <v-footer
+      app
+      class="pa-3"
+    >
+      <p class="ma-0">© {{ author }}</p>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import { author } from '../package'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  data() {
+    return {
+      author
+    }
+  }
 }
 </script>
